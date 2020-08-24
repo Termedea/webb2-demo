908d737c4539ab0b0a41444ede5eead5951d0bbe
Mikaela Hedberg <mikaela.hedberg@plandigital.se>
2020-08-24 16:51:00 +0200

    scroll effect

--- res/script/main.js
+++ res/script/main.js
@@ -1,4 +1,30 @@
-var scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 1000,
-    easing: 'easeInOutCubic'
-});
+/*** Bibliotek ***/
+var throttle = require("lodash/throttle");
+var debounce = require("lodash/debounce");
+
+/*** Variabler ***/
+var pageSection = document.querySelectorAll(".page-section");
+var navMainMenu = document.querySelector("nav.main-menu");
+var scrollClass = "scrolled";
+var browserHeight = window.innerHeight;
+
+/* Events */
+window.addEventListener("scroll", throttle(scrolling, 300)); //Anropar funktionen "scrolling" varje 200:e ms (skit i throttle, det är något smart som någon annan har gjort)
+window.addEventListener("resize", debounce(function(){
+    browserHeight = window.innerHeight;
+}), 333); //Ser till att browserHeight-variabeln uppdateras då och då (skit i debouce, det är något smart som någon annan har gjort)
+
+
+
+/*** Egna funktioner ***/
+function scrolling() {
+    if (window.scrollY > 100) {
+        navMainMenu.classList.add(scrollClass);
+
+    } else {
+        navMainMenu.classList.remove(scrollClass);
+    }
+    
+        
+}
+
