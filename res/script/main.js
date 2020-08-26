b0836676dd83fde7c51f9501c699debf0867fe65
Mikaela Hedberg <mikaela.hedberg@plandigital.se>
2020-08-26 12:45:46 +0200

    hover effects, comments

--- res/script/main.js
+++ res/script/main.js
@@ -1,33 +1,25 @@
 /*** Bibliotek ***/
 var throttle = require("lodash/throttle");
-var debounce = require("lodash/debounce");
+//Vet inte om vi behöver det, men det kan vara bra att ha. 
+var $ = require("jquery");
 
 /*** Variabler ***/
-var navMainMenu = document.querySelector("nav.main-menu");
+var navMainMenu = $("nav.main-menu");
 var scrollClass = "scrolled";
-var historyElement = document.querySelectorAll(".history .flex-item");
-console.log(historyElement);
+
 
 /* Events */
-window.addEventListener("scroll", throttle(scrolling, 300)); //Anropar funktionen "scrolling" varje 200:e ms (skit i throttle, det är något smart som någon annan har gjort)
-historyElement.addEventListener("click", function() {
-    console.log("clicked");
-});
+$(window).scroll(throttle(scrolling, 300)); //Anropar funktionen "scrolling" varje 200:e ms (skit i throttle, det är något smart som någon annan har gjort)
+
 
 /*** Egna funktioner ***/
 
 /** Ändrar utseende på menyn efter viss scroll **/
 function scrolling() {
     if (window.scrollY > 100) {
-        navMainMenu.classList.add(scrollClass);
+        navMainMenu.addClass(scrollClass);
 
     } else {
-        navMainMenu.classList.remove(scrollClass);
+        navMainMenu.removeClass(scrollClass);
     }
-}
-
-/** Kanske gör något kul med diskografi-popup **/
-
-function popup () {
-    console.log("clicked");
 }
